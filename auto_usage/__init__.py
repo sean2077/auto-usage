31ed94f46522ad05e39860f8e8eb6d2866e4b895
zhangxianbing <zhangxianbing09@163.com>
2021-05-19 14:20:36 +0800

    feat: first work version

---
+++ auto_usage/__init__.py
@@ -0,0 +1,5 @@
+"""Auto Usage - A python tool for auto-generating or auto-updating usage of python cli tools."""
+
+__version__ = "0.1.0"
+__author__ = """zhangxianbing"""
+__email__ = "zhangxianbing09@163.com"
